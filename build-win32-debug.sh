fdc4c3d32d253a470c5ef1f1709f56f1b3385315
cc <xu_yanfeng@126.com>
2023-03-10 13:56:19 +0800

    构建脚本

---
+++ build-win32-debug.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+echo "make clean ..."
+make clean
+echo "configure ..."
+./configure \
+--prefix=./output-win32-debug \
+--enable-gpl \
+--enable-small \
+--enable-shared \
+--enable-cross-compile \
+--enable-x86asm \
+--toolchain=msvc \
+--disable-postproc \
+--disable-debug \
+--disable-doc \
+--disable-ffplay \
+--disable-ffprobe \
+--disable-avdevice \
+--disable-symver
+
+# echo "makeing ..."
+# make -j6 #改成你的CPU核心数，6表示编译时占用6核
+# make install
